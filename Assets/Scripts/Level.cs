d853c68c072abc48afc178ca230c37932c0593f5
Thaminator <jjwtham@gmail.com>
2020-05-11 03:04:07 +0100

    Add Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("Begin");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("End");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
